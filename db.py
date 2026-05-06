ab730bcfe3411e10563e892b3707d5bf600a47d8
Roman <roman@MacBook-Air-Roman.local>
2026-05-06 11:13:17 +0300

    fix create tables

--- db.py
+++ db.py
@@ -15,41 +15,41 @@ async def init_db():
     async with pool.acquire() as conn:
         result = await conn.fetchval("SELECT 1")
         print("DB CONNECTED:", result)
+        
+        await conn.execute("""
+        CREATE TABLE IF NOT EXISTS clans (
+            id SERIAL PRIMARY KEY,
+            public_id BIGINT UNIQUE,
+            name TEXT,
+            owner_id BIGINT,
+            owner_name TEXT
+        );
+        """)
+
+        await conn.execute("""
+        CREATE TABLE IF NOT EXISTS clan_requests (
+            id SERIAL PRIMARY KEY,
+            user_id BIGINT,
+            username TEXT,
+            clan_id BIGINT,
+            status TEXT DEFAULT 'pending'
+        );
+        """)
+
+        await conn.execute("""
+        CREATE TABLE IF NOT EXISTS clan_members (
+            id SERIAL PRIMARY KEY,
+            user_id BIGINT,
+            username TEXT,
+            clan_id BIGINT,
+            role TEXT DEFAULT 'member',
+            dkp INTEGER DEFAULT 0
+        );
+        """)
+
 
 def get_pool():
     if pool is None:
         raise RuntimeError("DB is not initialized")
     return pool
 
-async with pool.acquire() as conn:
-
-    await conn.execute("""
-    CREATE TABLE IF NOT EXISTS clans (
-        id SERIAL PRIMARY KEY,
-        public_id BIGINT UNIQUE,
-        name TEXT,
-        owner_id BIGINT,
-        owner_name TEXT
-    );
-    """)
-
-    await conn.execute("""
-    CREATE TABLE IF NOT EXISTS clan_requests (
-        id SERIAL PRIMARY KEY,
-        user_id BIGINT,
-        username TEXT,
-        clan_id BIGINT,
-        status TEXT DEFAULT 'pending'
-    );
-    """)
-
-    await conn.execute("""
-    CREATE TABLE IF NOT EXISTS clan_members (
-        id SERIAL PRIMARY KEY,
-        user_id BIGINT,
-        username TEXT,
-        clan_id BIGINT,
-        role TEXT DEFAULT 'member',
-        dkp INTEGER DEFAULT 0
-    );
-    """)
