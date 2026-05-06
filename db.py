32904a0cd83a97a7d5a2b96bf2f55ff9e86eb450
Roman <roman@MacBook-Air-Roman.local>
2026-05-06 09:24:58 +0300

    fix pool

--- db.py
+++ db.py
@@ -2,14 +2,21 @@ import os
 import asyncpg
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-print("DATABASE_URL =", DATABASE_URL)
 pool = None
-
 
 async def init_db():
     global pool
+
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL is not set")
+
     pool = await asyncpg.create_pool(DATABASE_URL)
 
     async with pool.acquire() as conn:
         result = await conn.fetchval("SELECT 1")
         print("DB CONNECTED:", result)
+
+def get_pool():
+    if pool is None:
+        raise RuntimeError("DB is not initialized")
+    return pool
